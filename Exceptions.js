9490e9206eb7dc799f8afda5a53ae8e411b197e8
mirkjj <l7saienkoinna@gmail.com>
2024-08-25 16:19:12 +0300

    Exceptions handling practice

---
+++ Exceptions.js
@@ -0,0 +1,12 @@
+let d=0;
+try{
+    console.log (a);
+    let a = 3;
+    let c = a/d;
+}
+catch{
+    console.log("let must be declared");
+    if(d===0){
+        console.log('cannot be divided by zero');
+    }
+}
